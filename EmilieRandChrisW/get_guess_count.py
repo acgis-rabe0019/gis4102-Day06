1cfa6bc78c19376a2a4c8622e5b30d71fed1c70d
LAPTOP-KV4DTCCF\chris <wenk0003@algonquinlive.com>
2017-10-10 16:44:08 -0400

    Chris works on: Emilie works on:

---
+++ EmilieRandChrisW/get_guess_count.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      chris
+#
+# Created:     10-10-2017
+# Copyright:   (c) chris 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import random
+
+def get_guess_count(number_to_guess, min_bound, max_bound):
+    number=
+    guesses=0
